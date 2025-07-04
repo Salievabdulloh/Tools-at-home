dc8df5e6bf1e62bd966397d516d8ed65fca7b62e
Saliev_Abdulloh_07 <jonsonme771@gmail.com>
2025-07-04 06:30:58 +0500

    fixed some errors

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react"
 import { Route, Routes } from "react-router"
-import { Page2, Home, Layout } from "./pages/lazy/lazy"
+import { Page2,Page3,Page4,Page5,Page6,Page6,Page7,Page8, Home, Layout } from "./pages/lazy/lazy"
 
 function App() {
   return (
@@ -9,6 +9,12 @@ function App() {
         <Route path="/" element={<Suspense><Layout /></Suspense>}>
           <Route index element={<Suspense><Home /></Suspense>} />
           <Route path="/page2" element={<Suspense><Page2 /></Suspense>} />
+          <Route path="/page3" element={<Suspense><Page3 /></Suspense>} />
+          <Route path="/page4" element={<Suspense><Page4 /></Suspense>} />
+          <Route path="/page5" element={<Suspense><Page5 /></Suspense>} />
+          <Route path="/page6" element={<Suspense><Page6 /></Suspense>} />
+          <Route path="/page7" element={<Suspense><Page7 /></Suspense>} />
+          <Route path="/page8" element={<Suspense><Page8 /></Suspense>} />
         </Route>
       </Routes>
     </>
